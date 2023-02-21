8b63f3fe17493006a5a0e891a9a8096a4b89a66f
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-02-21 20:38:05 +0100

    given and then done

--- src/main/java/steps/LoginSteps.java
+++ src/main/java/steps/LoginSteps.java
@@ -1,2 +1,40 @@
-package steps;public class LoginSteps {
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import java.util.concurrent.TimeUnit;
+
+public class LoginSteps {
+
+    WebDriver driver;
+    @Given("Navigate to Page Phone Book")
+        public void navigateToLoginPage(){
+        System.setProperty("webdriver.chrome.driver", "D:\\2 tel run\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://phonebook.telran-edu.de:8080/user/login");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+    }
+    @When("Register new account link")
+    public void clickOnRegistrationLink(){
+        click(By.xpath("//a[contains(text(),'Register new Account')]"));
+    }
+
+    @Then("Registration form should appear")
+    public void isRegistrationFormPresent(){
+        Assert.assertTrue(isElementPresent(By.xpath("//h3[contains(text(),'Registration page')]")));
+    }
+
+    public boolean isElementPresent(By locator){
+        return driver.findElements(locator).size()>0;
+    }
+    private void click(By xpath) {
+    }
+
 }
---
+++ src/test/resources/features/navigate.feature
@@ -0,0 +1,7 @@
+Feature: Navigate
+
+  //@navigate
+  Scenario: Open Login Page
+    Given Navigate to Page Phone Book
+    When Register new account link
+    Then Registration form should appear
